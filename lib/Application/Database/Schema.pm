8355c7d486cfbeec629e718e9b64968ac82ff037
Paul Maskelyne <muttley@muttleyville.org>
2021-08-14 22:23:27 +0100

    start of schema

--- lib/Application/Database/Schema.pm
+++ lib/Application/Database/Schema.pm
@@ -10,6 +10,24 @@ use namespace::clean -except => [qw(meta)];
 sub UPGRADE_TO_1 {
 	return q(BEGIN TRANSACTION;
 
+CREATE TABLE campaign (
+	id       INTEGER PRIMARY KEY AUTOINCREMENT,
+	base_dir TEXT NOT NULL,
+	name     TEXT NOT NULL,
+
+	UNIQUE (base_dir, name)
+);
+
+CREATE TABLE files (
+	id          INTEGER PRIMARY KEY AUTOINCREMENT,
+	campaign_id INTEGER NOT NULL,
+	file_path   TEXT_NOT_NULL,
+	modified    INTEGER NOT NULL,
+	size        INTEGER NOT NULL,
+	hash        TEXT NOT NULL,
+
+	UNIQUE(campaign_id, file_path)
+)
 COMMIT;
 );
 }
